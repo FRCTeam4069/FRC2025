ae8b0568702cc25e01c07967cae05343f589dd90
John Liao <johnliao5@gmail.com>
2025-01-11 12:33:33 -0500

    try push again

--- src/main/java/frc/robot/wrappers/Motor.java
+++ src/main/java/frc/robot/wrappers/Motor.java
@@ -1,23 +1,16 @@
 package frc.robot.wrappers;
 
-import com.revrobotics.spark.SparkMax;      // can't find cansparkmax
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkBase;
 
 
 abstract class Motor {
-    public enum Controller {MAX, FLEX};      // anymore, should it be in constants?
+    double maxPower = 0.5;
 
-    private SparkBase motor;
-
-    double maxPower = 0.5;               // ?
-
-    // set power
-    void set (double power) {
-        motor.set(Math.max(-maxPower, power));
+    public double powerLimit(double p) {
+        return Math.max(Math.min(p, maxPower), -maxPower);
     }
 }
